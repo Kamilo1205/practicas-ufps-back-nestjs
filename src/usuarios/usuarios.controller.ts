ae4bc74a78c7d7f2105ffbe5a7fa512e56772443
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-19 12:30:04 -0500

    cambiar nombre del enum rol en el guard

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -11,38 +11,38 @@ import {
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { Role } from '../auth/enums/role.enum';
+import { Rol } from '../auth/enums/rol.enum';
 import { Roles } from 'src/auth/decorators';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
-  @Roles(Role.Coordinador)
+  @Roles(Rol.Coordinador)
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
   }
 
-  @Roles(Role.Coordinador)
+  @Roles(Rol.Coordinador)
   @Get()
   findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
     return this.usuariosService.findAll(page, limit);
   }
 
-  @Roles(Role.Coordinador)
+  @Roles(Rol.Coordinador)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usuariosService.findOne(id);
   }
 
-  @Roles(Role.Coordinador)
+  @Roles(Rol.Coordinador)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuariosService.update(id, updateUsuarioDto);
   }
 
-  @Roles(Role.Coordinador)
+  @Roles(Rol.Coordinador)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usuariosService.remove(id);
