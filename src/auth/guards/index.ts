9e4558a8c6813634e403fd86c0371e9016e36ba0
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-19 16:12:37 -0500

    agregar permisosGuard en archivo barril

--- src/auth/guards/index.ts
+++ src/auth/guards/index.ts
@@ -1,5 +1,6 @@
 export { GoogleOauthGuard } from './google-oauth.guard';
 export { JwtAuthGuard } from './jwt-auth.guard';
-export { LocalAuthGuard } from './local-auth.guard';
-export { RolesGuard } from './roles.guard';
 export { JwtRefreshGuard } from './jwt-refresh.guard';
+export { LocalAuthGuard } from './local-auth.guard';
+export { PermisosGuard } from './permisos.guard';
+export { RolesGuard } from './roles.guard';
