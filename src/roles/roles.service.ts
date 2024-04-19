0f970425467f37588611f96576eda9d765dee8ca
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-18 19:26:38 -0500

    cambiar el nombre de la entidad role a rol

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -2,13 +2,13 @@ import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateRoleDto, UpdateRoleDto } from './dto';
-import { Role } from './entities/role.entity';
+import { Rol } from './entities/rol.entity';
 
 @Injectable()
 export class RolesService {
   constructor(
-    @InjectRepository(Role)
-    private readonly rolesRepository: Repository<Role>,
+    @InjectRepository(Rol)
+    private readonly rolesRepository: Repository<Rol>,
   ) {}
 
   create(createRoleDto: CreateRoleDto) {
