11d7f634aa6977df44e20d4e1183ebbca9dd1c1a
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-19 14:55:10 -0500

    crear configuraciones para proteger rutas por permisos

---
+++ src/auth/guards/permisos.guard.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class PermisosGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredPermisos = this.reflector.get<string[]>(
+      'permisos',
+      context.getHandler(),
+    );
+    if (!requiredPermisos) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    const hasPermission = requiredPermisos.every((permiso) =>
+      user.permisos?.includes(permiso),
+    );
+    if (!hasPermission) {
+      throw new UnauthorizedException(
+        'Accesso Denegado: Permisos insuficientes',
+      );
+    }
+    return true;
+  }
+}
