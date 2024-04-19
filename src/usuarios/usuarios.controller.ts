1ace807ac381796c317d88ccce1e50a38a765234
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-19 17:11:30 -0500

    agregar guard de permisos

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -11,38 +11,43 @@ import {
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto, UpdateUsuarioDto } from './dto';
 import { Rol } from '../auth/enums/rol.enum';
-import { Roles } from 'src/auth/decorators';
+import { Permisos, Roles } from 'src/auth/decorators';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
-  @Roles(Rol.Coordinador)
   @Post()
+  @Roles(Rol.Coordinador)
+  @Permisos('crear-usuario')
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
   }
 
-  @Roles(Rol.Coordinador)
   @Get()
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-usuarios')
   findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
     return this.usuariosService.findAll(page, limit);
   }
 
-  @Roles(Rol.Coordinador)
   @Get(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-usuario')
   findOne(@Param('id') id: string) {
     return this.usuariosService.findOne(id);
   }
 
-  @Roles(Rol.Coordinador)
   @Patch(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('actualizar-usuario')
   update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuariosService.update(id, updateUsuarioDto);
   }
 
-  @Roles(Rol.Coordinador)
   @Delete(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('remover-usuario')
   remove(@Param('id') id: string) {
     return this.usuariosService.remove(id);
   }
