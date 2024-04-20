e7dc565913c362f327395553242a9b2c8ad9b9ea
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-20 18:37:33 -0500

    ocultar la propiedad fechaEliminacion si esta en null

--- src/roles/entities/rol.entity.ts
+++ src/roles/entities/rol.entity.ts
@@ -9,6 +9,7 @@ import {
 } from 'typeorm';
 import { Usuario } from 'src/usuarios/entities/usuario.entity';
 import { Permiso } from 'src/permisos/entities/permiso.entity';
+import { Transform } from 'class-transformer';
 
 @Entity()
 export class Rol {
@@ -19,6 +20,7 @@ export class Rol {
   nombre: string;
 
   @DeleteDateColumn()
+  @Transform(({ value }) => (value ? value : undefined))
   fechaEliminacion?: Date;
 
   //Relaciones
