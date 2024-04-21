dcb2e792dd5f05c5ab04e93d14c948d51f7344a7
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-21 09:56:07 -0500

    agegar permisos y roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -9,32 +9,44 @@ import {
 } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto, UpdateRoleDto } from './dto';
+import { Permisos, Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums/rol.enum';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
+  @Roles(Rol.Coordinador)
+  @Permisos('crear-rol')
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.rolesService.create(createRoleDto);
   }
 
   @Get()
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-roles')
   findAll() {
     return this.rolesService.findAll();
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-rol')
   findOne(@Param('id') id: string) {
     return this.rolesService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('actualizar-rol')
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.rolesService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('remover-rol')
   remove(@Param('id') id: string) {
     return this.rolesService.remove(id);
   }
