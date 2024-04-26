6fa831388de2d20c9d7fb863e81876ac706f95ff
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-25 20:31:03 -0500

    cambiar dto de uuid a uno general

--- src/areas-interes/areas-interes.controller.ts
+++ src/areas-interes/areas-interes.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AreasInteresService } from './areas-interes.service';
 import { CreateAreaInteresDto, UpdateAreaInteresDto } from './dto';
-import { FindAreaInteresDto } from './dto/find-area-interes.dto';
+import { UuidDto } from '../common/dto';
 
 @Controller('areas-interes')
 export class AreasInteresController {
@@ -18,17 +18,17 @@ export class AreasInteresController {
   }
 
   @Get(':id')
-  findOne(@Param() { id }: FindAreaInteresDto) {
+  findOne(@Param() { id }: UuidDto) {
     return this.areasInteresService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param() { id }: FindAreaInteresDto, @Body() updateAreaInteresDto: UpdateAreaInteresDto) {
+  update(@Param() { id }: UuidDto, @Body() updateAreaInteresDto: UpdateAreaInteresDto) {
     return this.areasInteresService.update(id, updateAreaInteresDto);
   }
 
   @Delete(':id')
-  remove(@Param() { id }: FindAreaInteresDto) {
+  remove(@Param() { id }: UuidDto) {
     return this.areasInteresService.remove(id);
   }
 }
