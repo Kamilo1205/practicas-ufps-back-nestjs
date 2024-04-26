3ca7ecc3b0266d23ba16e2356820aa6e7842b052
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-25 21:11:39 -0500

    transformar page y limit a string

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,11 +1,14 @@
-import { IsInt, IsNumber, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsInt, Min } from 'class-validator';
 
 export class PaginationDto {
   @IsInt()
   @Min(1)
+  @Type(() => Number)
   page: number;
   
   @IsInt()
   @Min(1)
+  @Type(() => Number)
   limit: number;  
 }
