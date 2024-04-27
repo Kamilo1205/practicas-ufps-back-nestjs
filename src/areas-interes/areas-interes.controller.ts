2a1447c1addf24a1cc12f4113923516bf284e09e
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-27 16:47:59 -0500

    modificar validador de uuid

--- src/areas-interes/areas-interes.controller.ts
+++ src/areas-interes/areas-interes.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AreasInteresService } from './areas-interes.service';
 import { CreateAreaInteresDto, UpdateAreaInteresDto } from './dto';
+import { UuidDto } from '../common/dto';
 
 @Controller('areas-interes')
 export class AreasInteresController {
@@ -17,17 +18,17 @@ export class AreasInteresController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
+  findOne(@Param() { id }: UuidDto) {
     return this.areasInteresService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateAreaInteresDto: UpdateAreaInteresDto) {
+  update(@Param() { id }: UuidDto, @Body() updateAreaInteresDto: UpdateAreaInteresDto) {
     return this.areasInteresService.update(id, updateAreaInteresDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
+  remove(@Param() { id }: UuidDto) {
     return this.areasInteresService.remove(id);
   }
 }
