99f0608dac2a75e4023222419ad6a378f745a66c
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-29 12:01:04 -0500

    agregar roles y permisos

--- src/tipo-afiliacion-eps/tipo-afiliacion-eps.controller.ts
+++ src/tipo-afiliacion-eps/tipo-afiliacion-eps.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CreateTipoAfiliacionEpsDto, UpdateTipoAfiliacionEpsDto } from './dto';
 import { TipoAfiliacionEpsService } from './tipo-afiliacion-eps.service';
 import { UuidDto } from 'src/common/dto';
+import { Permisos, Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums/rol.enum';
 
 @Controller('tipo-afiliacion-eps')
 export class TipoAfiliacionEpsController {
   constructor(private readonly tipoAfiliacionEpsService: TipoAfiliacionEpsService) {}
 
   @Post()
+  @Roles(Rol.Coordinador)
+  @Permisos('crear-tipo-afiliacion-eps')
   create(@Body() createTipoAfiliacionEpsDto: CreateTipoAfiliacionEpsDto) {
     return this.tipoAfiliacionEpsService.create(createTipoAfiliacionEpsDto);
   }
@@ -18,16 +22,22 @@ export class TipoAfiliacionEpsController {
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-tipo-afiliacion-eps')
   findOne(@Param() { id }: UuidDto) {
     return this.tipoAfiliacionEpsService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('actualizar-tipo-afiliacion-eps')
   update(@Param() { id }: UuidDto, @Body() updateTipoAfiliacionEpDto: UpdateTipoAfiliacionEpsDto) {
     return this.tipoAfiliacionEpsService.update(id, updateTipoAfiliacionEpDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('remover-tipo-afiliacion-eps')
   remove(@Param() { id }: UuidDto) {
     return this.tipoAfiliacionEpsService.remove(id);
   }
