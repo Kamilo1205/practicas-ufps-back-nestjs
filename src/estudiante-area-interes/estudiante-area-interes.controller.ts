4f1b61462a4afeb44e1dd63f121f8cb64354c36b
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-29 12:05:24 -0500

    agregar roles y permisos

--- src/estudiante-area-interes/estudiante-area-interes.controller.ts
+++ src/estudiante-area-interes/estudiante-area-interes.controller.ts
@@ -2,33 +2,45 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EstudianteAreaInteresService } from './estudiante-area-interes.service';
 import { CreateEstudianteAreaIntereDto, UpdateEstudianteAreaIntereDto } from './dto/';
 import { UuidDto } from 'src/common/dto';
+import { Permisos, Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums/rol.enum';
 
 @Controller('estudiante-area-interes')
 export class EstudianteAreaInteresController {
   constructor(private readonly estudianteAreaInteresService: EstudianteAreaInteresService) {}
 
   @Post()
+  @Roles(Rol.Coordinador)
+  @Permisos('crear-estudiante-area-interes')
   create(@Body() createEstudianteAreaIntereDto: CreateEstudianteAreaIntereDto) {
     return this.estudianteAreaInteresService.create(createEstudianteAreaIntereDto);
   }
 
   @Get()
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-estudiantes-areas-interes')
   findAll() {
     return this.estudianteAreaInteresService.findAll();
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('obtener-estudiante-areas-interes')
   findOne(@Param() { id }: UuidDto) {
     return this.estudianteAreaInteresService.findOne(id);
   }
 
-  /* @Patch(':id')
+  @Patch(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('actualizar-estudiante-area-interes')
   update(@Param() { id }: UuidDto, @Body() updateEstudianteAreaIntereDto: UpdateEstudianteAreaIntereDto) {
     return this.estudianteAreaInteresService.update(id, updateEstudianteAreaIntereDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador)
+  @Permisos('remover-estudiante-area-interes')
   remove(@Param() { id }: UuidDto) {
     return this.estudianteAreaInteresService.remove(id);
-  } */
+  }
 }
