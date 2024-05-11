26f75c86cbd298bbdd281e84f036df9cb71b15a1
Kamilo1205 <kamilo_1205@outlook.com>
2024-05-11 05:44:45 -0500

    actualizar entidad

--- src/tipo-documento/entities/tipo-documento.entity.ts
+++ src/tipo-documento/entities/tipo-documento.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, OneToMany } from 'typeorm';
+import { RepresentanteLegal } from 'src/empresas/entities/representante-legal.entity';
 import { Transform } from 'class-transformer';
 
 @Entity()
@@ -18,4 +19,8 @@ export class TipoDocumento {
   @DeleteDateColumn({ type: 'date' })
   @Transform(({ value }) => (value ? value : undefined))
   fechaEliminacion: Date;
+
+  // Relaciones
+  @OneToMany(() => RepresentanteLegal, representanteLegal => representanteLegal.tipoDocumento)
+  representantesLegales: RepresentanteLegal[];
 }
