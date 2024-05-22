cf4bad8defae9ff4824a24da953fe6f458a5065e
Kamilo1205 <kamilo_1205@outlook.com>
2024-05-22 07:38:56 -0500

    agregar seeder

--- src/seeder/seeder.controller.ts
+++ src/seeder/seeder.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeederService } from './seeder.service';
+import { Public } from 'src/auth/decorators';
 
 @Controller('seeder')
 export class SeederController {
   constructor(private readonly seederService: SeederService) {}
 
   @Get()
+  @Public()
   async seed() {
     await this.seederService.seed();
     return { message: 'Seeding complete!' };
