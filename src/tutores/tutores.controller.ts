5236a7930a34ebcfa0d97591e83349a2e8cd662a
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-12 07:11:09 -0500

    agregar permisos al rol administrador

--- src/tutores/tutores.controller.ts
+++ src/tutores/tutores.controller.ts
@@ -2,32 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { TutoresService } from './tutores.service';
 import { CreateTutoreDto } from './dto/create-tutor.dto';
 import { UpdateTutoreDto } from './dto/update-tutor.dto';
+import { Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums';
 
 @Controller('tutores')
 export class TutoresController {
   constructor(private readonly tutoresService: TutoresService) {}
 
   @Post()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   create(@Body() createTutoreDto: CreateTutoreDto) {
     return this.tutoresService.create(createTutoreDto);
   }
 
   @Get()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findAll() {
     return this.tutoresService.findAll();
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOne(@Param('id') id: string) {
     return this.tutoresService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   update(@Param('id') id: string, @Body() updateTutoreDto: UpdateTutoreDto) {
     return this.tutoresService.update(+id, updateTutoreDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   remove(@Param('id') id: string) {
     return this.tutoresService.remove(+id);
   }
