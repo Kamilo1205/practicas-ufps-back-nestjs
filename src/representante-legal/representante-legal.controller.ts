b96155db5a8858ec348645a323fcb31d6363efe5
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-12 07:10:02 -0500

    agregar permisos al rol administrador

--- src/representante-legal/representante-legal.controller.ts
+++ src/representante-legal/representante-legal.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RepresentanteLegalService } from './representante-legal.service';
 import { CreateRepresentanteLegalDto, UpdateRepresentanteLegalDto } from './dto';
 import { UuidDto } from 'src/common/dto';
+import { Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums';
 
 @Controller('representante-legal')
 export class RepresentanteLegalController {
   constructor(private readonly representanteLegalService: RepresentanteLegalService) {}
 
   @Post()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   create(@Body() createRepresentanteLegalDto: CreateRepresentanteLegalDto) {
     return this.representanteLegalService.create(createRepresentanteLegalDto);
   }
@@ -18,16 +21,19 @@ export class RepresentanteLegalController {
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOne(@Param() { id }: UuidDto) {
     return this.representanteLegalService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   update(@Param() { id }: UuidDto, @Body() updateRepresentanteLegalDto: UpdateRepresentanteLegalDto) {
     return this.representanteLegalService.update(id, updateRepresentanteLegalDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   remove(@Param() { id }: UuidDto) {
     return this.representanteLegalService.remove(id);
   }
