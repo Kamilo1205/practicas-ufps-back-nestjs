49de2f4f75c71e38e68e31030ee30a9fa5a37385
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-12 07:06:42 -0500

    agregar permisos al rol administrador

--- src/anio/anio.controller.ts
+++ src/anio/anio.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AnioService } from './anio.service';
 import { CreateAnioDto, UpdateAnioDto } from './dto/';
 import { UuidDto } from 'src/common/dto';
+import { Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums';
 
 @Controller('anio')
 export class AnioController {
   constructor(private readonly anioService: AnioService) {}
 
   @Post()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   create(@Body() createAnioDto: CreateAnioDto) {
     return this.anioService.create(createAnioDto);
   }
@@ -18,16 +21,19 @@ export class AnioController {
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOne(@Param() { id }: UuidDto) {
     return this.anioService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   update(@Param('id') { id }: UuidDto, @Body() updateAnioDto: UpdateAnioDto) {
     return this.anioService.update(id, updateAnioDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   remove(@Param('id') { id }: UuidDto) {
     return this.anioService.remove(id);
   }
