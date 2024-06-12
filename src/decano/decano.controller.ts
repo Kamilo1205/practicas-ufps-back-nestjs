d3baaf1e03e4f4cac84754a12d8752b550eaf265
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-12 07:07:43 -0500

    agregar permisos al rol administrador

--- src/decano/decano.controller.ts
+++ src/decano/decano.controller.ts
@@ -3,37 +3,45 @@ import { DecanoService } from './decano.service';
 import { CreateDecanoDto } from './dto/create-decano.dto';
 import { UpdateDecanoDto } from './dto/update-decano.dto';
 import { UuidDto } from 'src/common/dto';
+import { Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums';
 
 @Controller('decano')
 export class DecanoController {
   constructor(private readonly decanoService: DecanoService) {}
 
   @Post()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   create(@Body() createDecanoDto: CreateDecanoDto) {
     return this.decanoService.create(createDecanoDto);
   }
 
   @Get()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOne() {
     return this.decanoService.findOne();
   }
 
   @Get('/all')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findAll() {
     return this.decanoService.findAll();
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOneById(@Param() { id }: UuidDto) {
     return this.decanoService.findOneById(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   update(@Param() { id }: UuidDto, @Body() updateDecanoDto: UpdateDecanoDto) {
     return this.decanoService.update(id, updateDecanoDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   remove(@Param() { id }: UuidDto) {
     return this.decanoService.remove(id);
   }
