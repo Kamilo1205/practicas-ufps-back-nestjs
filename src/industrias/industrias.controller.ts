35ea4978a290c235ddc8a557e197bb62c7fb44f7
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-12 07:09:29 -0500

    agregar permisos al rol administrador

--- src/industrias/industrias.controller.ts
+++ src/industrias/industrias.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CreateIndustriaDto, UpdateIndustriaDto } from './dto';
 import { IndustriasService } from './industrias.service';
 import { UuidDto } from 'src/common/dto';
+import { Roles } from 'src/auth/decorators';
+import { Rol } from 'src/auth/enums';
 
 @Controller('industrias')
 export class IndustriasController {
   constructor(private readonly industriasService: IndustriasService) {}
 
   @Post()
+  @Roles(Rol.Coordinador, Rol.Administrador)
   create(@Body() createIndustriaDto: CreateIndustriaDto) {
     return this.industriasService.create(createIndustriaDto);
   }
@@ -18,16 +21,19 @@ export class IndustriasController {
   }
 
   @Get(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   findOne(@Param() { id }: UuidDto) {
     return this.industriasService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   update(@Param() { id }: UuidDto, @Body() updateIndustriaDto: UpdateIndustriaDto) {
     return this.industriasService.update(id, updateIndustriaDto);
   }
 
   @Delete(':id')
+  @Roles(Rol.Coordinador, Rol.Administrador)
   remove(@Param() { id }: UuidDto) {
     return this.industriasService.remove(id);
   }
