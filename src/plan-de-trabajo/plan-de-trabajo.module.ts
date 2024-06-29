006679c1a373d1b4a01e2ca0035163b5d3091cf5
Kamilo1205 <kamilo_1205@outlook.com>
2024-06-29 18:00:19 -0500

    modificar plan de trabajo

--- src/plan-de-trabajo/plan-de-trabajo.module.ts
+++ src/plan-de-trabajo/plan-de-trabajo.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlanDeTrabajoService } from './plan-de-trabajo.service';
 import { PlanDeTrabajoController } from './plan-de-trabajo.controller';
 import { PlanDeTrabajo } from './entities/plan-de-trabajo.entity';
+import { EstudiantesModule } from 'src/estudiantes/estudiantes.module';
+import { IntensidadHoraria } from './entities/intensidad-horaria.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlanDeTrabajo])],
+  imports: [
+    TypeOrmModule.forFeature([PlanDeTrabajo, IntensidadHoraria]),
+    EstudiantesModule
+  ],
   controllers: [PlanDeTrabajoController],
   providers: [PlanDeTrabajoService],
   exports: [PlanDeTrabajoService]
