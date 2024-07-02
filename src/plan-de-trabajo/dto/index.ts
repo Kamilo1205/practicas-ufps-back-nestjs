6ed7542cbf8f6061c3de143f00a3a18ff9b364e8
Kamilo1205 <kamilo_1205@outlook.com>
2024-07-01 20:22:58 -0500

    eliminar archivos de intensidad-horaria

--- src/plan-de-trabajo/dto/index.ts
+++ src/plan-de-trabajo/dto/index.ts
@@ -1,4 +1,2 @@
 export { CreatePlanDeTrabajoDto } from './create-plan-de-trabajo.dto';
 export { UpdatePlanDeTrabajoDto } from './update-plan-de-trabajo.dto';
-export { CreateIntensidadHorarioDto } from './create-intensidad-horaria.dto';
-export { UpdateIntensidadHorarioDto } from './update-intensidad-horaria.dto';
