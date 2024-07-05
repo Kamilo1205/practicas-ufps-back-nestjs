013ad1d1f8f3f0b62939ef55f2aa6e0cfcfa9add
Kamilo1205 <kamilo_1205@outlook.com>
2024-07-04 21:59:08 -0500

    agregar entidad director

--- src/director/entities/director.entity.ts
+++ src/director/entities/director.entity.ts
@@ -1,7 +1,8 @@
-import { Column, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { Usuario } from 'src/usuarios/entities/usuario.entity';
 
+@Entity()
 export class Director extends BaseEntity {
   @Column()
   nombres: string;
