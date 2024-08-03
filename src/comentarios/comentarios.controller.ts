279f80fe56aacd51fe5f668359ada0b8dbd1862e
Kamilo1205 <kamilo_1205@outlook.com>
2024-08-02 22:46:23 -0500

    agregar roles

--- src/comentarios/comentarios.controller.ts
+++ src/comentarios/comentarios.controller.ts
@@ -10,19 +10,19 @@ export class ComentariosController {
   constructor(private readonly comentariosService: ComentariosService) {}
 
   @Post()
-  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador)
+  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador, Rol.Estudiante)
   create(@Body() createComentarioDto: CreateComentarioDto, @GetUser() usuario: Usuario) {
     return this.comentariosService.create(createComentarioDto, usuario);
   }
 
   @Patch(':id')
-  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador)
+  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador, Rol.Estudiante)
   update(@Param('id') id: string, @Body() updateComentarioDto: UpdateComentarioDto, @GetUser() usuario: Usuario) {
     return this.comentariosService.update(id, updateComentarioDto, usuario);
   }
 
   @Delete(':id')
-  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador)
+  @Roles(Rol.Coordinador, Rol.Tutor, Rol.Administrador, Rol.Estudiante)
   remove(@Param('id') id: string, @GetUser() usuario: Usuario) {
     return this.comentariosService.remove(id, usuario);
   }
