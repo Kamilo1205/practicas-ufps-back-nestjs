dc0ac3397e9bf7d288f52b72d229c18fc3c47388
Kamilo1205 <kamilo_1205@outlook.com>
2024-08-06 23:35:38 -0500

    actualizar

--- src/plan-de-trabajo/dto/index.ts
+++ src/plan-de-trabajo/dto/index.ts
@@ -1,5 +1,3 @@
 export { CreatePlanDeTrabajoDto } from './create-plan-de-trabajo.dto';
 export { UpdatePlanDeTrabajoDto } from './update-plan-de-trabajo.dto';
-export { CreateResultadoDto } from './create-resultado.dto';
 export { CreateResultadosDto } from './create-resultados.dto';
-export { UpdateResultadoDto } from './update-resultado.dto';
