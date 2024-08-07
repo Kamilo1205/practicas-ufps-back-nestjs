40e18e3dbf87956198b87e4cdcc163f5f6941d74
Kamilo1205 <kamilo_1205@outlook.com>
2024-08-07 16:54:27 -0500

    actualizar

--- src/evaluacion-estudiante/dto/create-evaluacion-estudiante.dto.ts
+++ src/evaluacion-estudiante/dto/create-evaluacion-estudiante.dto.ts
@@ -3,23 +3,15 @@ import { Type } from 'class-transformer';
 
 export class CreateEvaluacionEstudianteDto {
   @IsArray()
-  @IsBoolean({ each: true })
-  @Type(() => Boolean)
   procesoDeGestion: boolean[][];
 
   @IsArray()
-  @IsBoolean({ each: true })
-  @Type(() => Boolean)
   jefeInmediato: boolean[][];
 
   @IsArray()
-  @IsBoolean({ each: true })
-  @Type(() => Boolean)
   empresa: boolean[][];
 
   @IsArray()
-  @IsBoolean({ each: true })
-  @Type(() => Boolean)
   aporteRealizacion: boolean[][];
 
   @IsString()
