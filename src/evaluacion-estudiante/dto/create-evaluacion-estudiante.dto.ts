341b3e92f5a705c1e2f816286ebe3e34aed2c56b
Kamilo1205 <kamilo_1205@outlook.com>
2024-08-07 14:27:01 -0500

    actualizar

--- src/evaluacion-estudiante/dto/create-evaluacion-estudiante.dto.ts
+++ src/evaluacion-estudiante/dto/create-evaluacion-estudiante.dto.ts
@@ -1,27 +1,23 @@
-import { IsArray, IsBoolean, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { IsArray, IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateEvaluacionEstudianteDto {
   @IsArray()
-  @IsArray({ each: true })
   @IsBoolean({ each: true })
   @Type(() => Boolean)
   procesoDeGestion: boolean[][];
 
   @IsArray()
-  @IsArray({ each: true })
   @IsBoolean({ each: true })
   @Type(() => Boolean)
   jefeInmediato: boolean[][];
 
   @IsArray()
-  @IsArray({ each: true })
   @IsBoolean({ each: true })
   @Type(() => Boolean)
   empresa: boolean[][];
 
   @IsArray()
-  @IsArray({ each: true })
   @IsBoolean({ each: true })
   @Type(() => Boolean)
   aporteRealizacion: boolean[][];
